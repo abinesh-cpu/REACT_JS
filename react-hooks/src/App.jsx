ad64fcf23c128fc93e76648542e9503a8e1e563a
abinesh-cpu <abineshrl476@gmail.com>
2025-03-13 13:10:53 +0530

    react hooks completed

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -137,24 +137,87 @@
 
 
 
-import React,{ useState,useMemo} from 'react';
+// import React,{ useState,useMemo} from 'react';
+
+// const App=()=>{
+//   const [count,setcount]=useState(0)
+//   const [text,settext]=useState('')
+
+//   const doubledvalue=useMemo(()=>{
+//     return count*2
+//   },[count])
+// return(
+//   <div>
+//     <h2>usememo example</h2>
+//     <p>count:{count}</p>
+//     <p>doubledvalue:{doubledvalue}</p>
+//     <button onClick={()=>setcount(count+1)}>increment</button>
+//     <br />
+//     <input type="text" value={text} onChange={(e)=>settext(e.target.value)} placeholder='type something......'/>
+//   </div>
+// )
+// }
+// export default App
+
+
+
+
+
+// import { useMemo,useState } from "react";
+
+// export default function App(){
+//   const [count, setCount] = useState(0);
+//   let sum=useMemo(()=>loop(),[])
+//   return(
+//     <>
+//       <p>{count}</p>
+//       <button onClick={()=>setCount(count+1)}>increment</button>
+//       <p>sum :{sum}</p>
+//     </>
+//   )
+// }
+// function loop(){
+//   let sum=0;
+//   for(let i=0;i<=1e9;i++){
+//     sum+=i;
+// }
+// return sum;
+// }
+
+
+
+
+
+import React,{useState,useCallback} from "react";
+
+const Counter=React.memo(({count1,increment1})=>{
+  console.log('child re-rendeered')
+  return(
+    <>
+      <p>second component</p>
+      <p>{count1}</p>
+      <button onClick={increment1}>increment</button>
+    </>
+  )
+})
 
 const App=()=>{
-  const [count,setcount]=useState(0)
-  const [text,settext]=useState('')
-
-  const doubledvalue=useMemo(()=>{
-    return count*2
-  },[count])
-return(
-  <div>
-    <h2>usememo example</h2>
-    <p>count:{count}</p>
-    <p>doubledvalue:{doubledvalue}</p>
-    <button onClick={()=>setcount(count+1)}>increment</button>
-    <br />
-    <input type="text" value={text} onChange={(e)=>settext(e.target.value)} placeholder='type something......'/>
-  </div>
-)
+  const [count, setCount] = useState(0);
+  const [count1,setCount1]=useState(0)
+  const increment=()=>{
+    setCount((prevcount)=>prevcount+1)
+  }
+  const increment1=useCallback(()=>{
+    setCount1((prevcount)=>prevcount+1)
+    },[count1])
+    console.log('parent rendered')
+    return(
+      <div>
+        <p>parent component</p>
+        <h2>count : {count}</h2>
+        <button onClick={increment}>count</button>
+        <Counter count1={count1} increment1={increment1}/>
+      </div>
+    )
 }
 export default App
